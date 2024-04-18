6c58311953a992d67ee34e85439b170ddd473b52
Sunil G. Gawai <sunilgawai7397@gmail.com>
2024-04-18 13:42:11 +0530

    theme provider initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Home from "./pages/home";
+
 export default function App() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }
+	return (
+		// <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+			<div className="bg-primary text-primary-foreground">
+				<h1 className="text-3xl font-bold underline">Hello world!</h1>
+				<Home />
+			</div>
+		// </ThemeProvider>
+	);
+}
